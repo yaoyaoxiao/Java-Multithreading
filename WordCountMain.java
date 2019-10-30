c9634a7f3a83f4c6d83414eb939d83891d0b8e10
Xiaofang Yu <xiaofangyu@Xiaofangs-MacBook-Air.local>
2019-10-29 22:09:27 -0700

    Fix to Synchronized statements

--- WordCountMain.java
+++ WordCountMain.java
@@ -1,11 +1,15 @@
+package Practice;
+
 import java.util.*;
 import java.io.*;
 
 class WordCounter implements Runnable{
     private Scanner sc;
     private final Map<String, Integer> map;
-    public WordCounter(String s, Map<String, Integer> map){
+    private Object mapLock;
+    public WordCounter(String s, Map<String, Integer> map , Object lock){
         this.map = map;
+        this.mapLock = lock;
         try {
             File f = new File(s);
             sc = new Scanner(f);
@@ -16,7 +20,7 @@ class WordCounter implements Runnable{
     public void run(){
         sc.useDelimiter(",");
 
-        synchronized (this) {
+        synchronized (mapLock) {
             while ( sc.hasNext()){
                 String cur = sc.next();
                 map.put(cur, map.getOrDefault(cur, 0) + 1);
@@ -25,28 +29,32 @@ class WordCounter implements Runnable{
     }
 }
 class ScheduledReport extends TimerTask{
-    private final Map<String, Integer> map;
-    public ScheduledReport(Map<String, Integer> map){
+    private Map<String, Integer> map;
+    private Object mapLock;
+    public ScheduledReport(Map<String, Integer> map, Object lock){
         this.map = map;
+        this.mapLock = lock;
     }
-    public void run(){ //should I use synchronized here too?
-        for  (Map.Entry<String, Integer> e : map.entrySet()){
-            System.out.println( e.getKey() + ": " + e.getValue());
+    public void run(){ //should I use synchronized here too? Yes
+        synchronized (mapLock){
+            for  (Map.Entry<String, Integer> e : map.entrySet()){
+                System.out.println( e.getKey() + ": " + e.getValue());
+            }
         }
     }
 }
 
 class WordCountMain{
-
-    public volatile Map<String, Integer> map;
     public static void main( String args[]) {
-        WordCountMain wcm = new WordCountMain();
-        wcm.map = new HashMap();
-        Thread t1 = new Thread (new WordCounter("file1.txt", wcm.map));
-        Thread t2 = new Thread (new WordCounter("file2.txt", wcm.map));
+        // WordCountMain wcm = new WordCountMain();
+        Map<String, Integer> map = new HashMap();
+        Object lock = new Object();
+
+        Thread t1 = new Thread (new WordCounter("/Developer/project/file1.txt", map, lock));
+        Thread t2 = new Thread (new WordCounter("/Developer/project/file2.txt", map, lock));
 
         Timer timer = new Timer();
-        timer.schedule( new ScheduledReport(wcm.map), 0, 2000);
+        timer.schedule( new ScheduledReport(map, lock), 0, 2000);
 
         t1.start();
         t2.start();
